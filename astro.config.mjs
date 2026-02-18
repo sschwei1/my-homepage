a69f5eeca662e88f6a71a1ec900336e094ceceda
sschwei1 <24sebastian05@gmail.com>
2026-02-18 15:33:27 +0100

    Add initial site structure

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    icon({
+      include: {
+        'fa6-brands': ['*'],
+      }
+    })
+  ]
+});
